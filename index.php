8979bd3538782f336c63a36a4fe447079a7207da
Matheus Fernandes <tecnologia.matheus@gmail.com>
2016-09-22 16:43:55 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.css">
+    <title>Cliednte</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <h1>Clientes</h1>
+        </div>
+    </div>
+
+<script src="bower_components/jquery/dist/jquery.min.js"></script>
+<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
